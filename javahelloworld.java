edf9a2fb49a13cd5ddb9297da6cfd3bdfbf494d2
rajeevvarma16 <varma410@gmail.com>
2016-03-27 22:54:04 -0700

    Adding dockerfile and java program to git repo

---
+++ javahelloworld.java
@@ -0,0 +1,9 @@
+public class JavaHelloWorld
+{
+    public static void main (String [] args)
+    {
+              System.out.println("Java hello world");
+              System.out.println("Hello again");
+     }
+}
+      
